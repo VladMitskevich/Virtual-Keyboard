4a96588a3eb51b0ecaa9f0d974b72b80700d2146
Vlad Mitskevich <vlad.mitskevich@mail.ru>
2022-05-06 16:30:33 +0300

    feat: add unworking language change

--- script.js
+++ script.js
@@ -5,18 +5,15 @@ window.addEventListener("DOMContentLoaded", function() {
                 'ShiftLeft','z','x','c','v','b','n','m',',','.','/','ArrowUp','ShiftRight',
                 'ControlLeft','Meta','AltLeft','Space','AltRight','ArrowLeft','ArrowDown','ArrowRight','ControlRight'];
 
-
-window.addEventListener('keydown', (event)=>{
-        if(event.shiftKey && event.altKey){
-          buttons = ['`','1','2','3','4','5','6','7','8','9','0','-','=','Backspace',
-                 'Tab','й','ц','у','к','е','н','г','ш','щ','з','х','ъ','\\',
+  let rusButtons = ['ё','1','2','3','4','5','6','7','8','9','0','-','=','Backspace',
+                 'Tab','й','ц','у','к','е','н','г','ш','щ','з','х','ъ','\\','Delete',
                 'CapsLock','ф','ы','в','а','п','р','о','л','д','ж','э','Enter',
-                'ShiftLeft','я','ч','с','м','и','т','ь','б','ю','ArrowUp','ShiftRight',
+                'ShiftLeft','я','ч','с','м','и','т','ь','б','ю','/','ArrowUp','ShiftRight',
                 'ControlLeft','Meta','AltLeft','Space','AltRight','ArrowLeft','ArrowDown','ArrowRight','ControlRight'];
-                console.log(buttons);
-        }
+               
+ let capsLock = false;   
         
-      })              
+               
   const body = document.querySelector('body');
   body.classList.add('body');
 
@@ -41,74 +38,89 @@ window.addEventListener('keydown', (event)=>{
    let keyboardKeys = document.createElement('div');
    keyboardKeys.classList.add('keyboard-keys');
    keyboard.appendChild(keyboardKeys);
+   
 
-   function addButtons(){
+   function addButtons(condition){
+      function useCapsLock(){
+      capsLock = !capsLock;
+      console.log(capsLock);
+      if(capsLock){
+        condition = condition.map(function(e) {return e.toUpperCase() })
+      }
+      else{
+        condition = condition.map(function(e) {return e.toLowerCase() })
+      }
+   }
+   
+     
     
-    for(let i= 0;i<buttons.length;i++){
+    for(let i= 0;i<condition.length;i++){
       const key = document.createElement('button');
-      const buttonsLineBreak = ['Backspace','Delete','Enter', 'ShiftRight'].indexOf(buttons[i]) !== -1;
+      const buttonsLineBreak = ['Backspace','Delete','Enter', 'ShiftRight'].indexOf(condition[i]) !== -1;
+      
 
+      
       
       key.setAttribute("type", "button");
       key.classList.add('keyboard-key');
-      key.textContent = buttons[i];
-      if(buttons[i] === 'Backspace'){
+      key.textContent = condition[i];
+      if(condition[i] === 'Backspace'){
         key.classList.add('wide-key');  
         key.innerHTML = createIcon("backspace");
       }
-      if(buttons[i] === 'Tab'){
+      if(condition[i] === 'Tab'){
         key.innerHTML = createIcon("keyboard_tab");
       }
-      if(buttons[i] === 'Delete'){
+      if(condition[i] === 'Delete'){
         key.classList.add('delete-key');
         key.textContent = 'Del';
       }
-      if(buttons[i] === 'CapsLock'){
+      if(condition[i] === 'CapsLock'){
         key.innerHTML = createIcon("keyboard_capslock");
         key.classList.add('super-wide-key');
       }
-      if(buttons[i] === 'Enter'){
+      if(condition[i] === 'Enter'){
         key.classList.add('super-wide-key');
       }
-      if(buttons[i] === 'AltLeft' || buttons[i] === 'AltRight'){
+      if(condition[i] === 'AltLeft' || buttons[i] === 'AltRight'){
         key.textContent = 'Alt';
       }
-      if(buttons[i] === 'ArrowRight'){
+      if(condition[i] === 'ArrowRight'){
         key.innerHTML = createIcon("keyboard_arrow_right");
       }
-      if(buttons[i] === 'ArrowLeft'){
+      if(condition[i] === 'ArrowLeft'){
         key.innerHTML = createIcon("keyboard_arrow_left");
       }
-      if(buttons[i] === 'ArrowDown'){
+      if(condition[i] === 'ArrowDown'){
         key.innerHTML = createIcon("keyboard_arrow_down");
       }
-      if(buttons[i] === 'ArrowUp'){
+      if(condition[i] === 'ArrowUp'){
         key.innerHTML = createIcon("keyboard_arrow_up");
       }
-      if(buttons[i] === 'Space'){
+      if(condition[i] === 'Space'){
         key.classList.add('space-key')
         key.innerHTML = createIcon("space_bar");
       }
-      if(buttons[i] === 'ShiftLeft'){
+      if(condition[i] === 'ShiftLeft'){
         key.classList.add('shift-key')
         key.textContent = 'Shift';
       }
-      if(buttons[i] === 'ShiftRight'){
+      if(condition[i] === 'ShiftRight'){
         key.classList.add('shiftRight-key')
         key.textContent = 'Shift';
       }
-      if(buttons[i] === 'ControlLeft'){
+      if(condition[i] === 'ControlLeft'){
         key.classList.add('ctrl-key')
         key.textContent = 'Ctrl';
       }
-      if(buttons[i] === 'ControlRight'){
+      if(condition[i] === 'ControlRight'){
         key.classList.add('ctrlRight-key')
         key.textContent = 'Ctrl';
       }
-      if(buttons[i] === 'Enter'){ 
+      if(condition[i] === 'Enter'){ 
         key.innerHTML = createIcon("keyboard_return");
       }
-      if(buttons[i] === 'Meta'){ 
+      if(condition[i] === 'Meta'){ 
         key.textContent = 'Win';
       }
 
@@ -120,7 +132,7 @@ window.addEventListener('keydown', (event)=>{
       //----------------------Чтение кнопок с клавиатуры----------------------------//
 
       document.addEventListener('keydown', (event)=>{
-        if(event.key === buttons[i] || event.code === buttons[i]){
+        if(event.key === condition[i] || event.code === condition[i]){
           key.classList.toggle("active");
           if(event.key === 'Backspace'){
             textarea.textContent = textarea.textContent.substring(0, textarea.textContent.length-1);
@@ -137,17 +149,20 @@ window.addEventListener('keydown', (event)=>{
           else if(event.key === 'Tab'){
             textarea.textContent += '   ';
           }
-          else if(buttons[i] === 'CapsLock' || buttons[i] === 'ControlRight' || buttons[i] === 'ControlLeft'|| buttons[i] === 'ShiftRight' || buttons[i] === 'ShiftLeft' || buttons[i] === 'AltRight' || buttons[i] === 'AltLeft' || buttons[i] === 'Meta' || buttons[i] === 'ArrowDown' || buttons[i] === 'ArrowUp' || buttons[i] === 'ArrowLeft' || buttons[i] === 'ArrowRight'){
-            textarea.textContent = textarea.textContent;
+          else if(condition[i] === 'ControlRight' || condition[i] === 'ControlLeft'|| condition[i] === 'ShiftRight' || condition[i] === 'ShiftLeft' || condition[i] === 'AltRight' || condition[i] === 'AltLeft' || condition[i] === 'Meta' || condition[i] === 'ArrowDown' || condition[i] === 'ArrowUp' || condition[i] === 'ArrowLeft' || condition[i] === 'ArrowRight'){
+            textarea.textContent += '';
             event.preventDefault();
           }
+          else if(condition[i] === 'CapsLock'){
+            useCapsLock();
+          }
           else{
-            textarea.textContent += buttons[i];
+            textarea.textContent += condition[i];
           }
         }
         
       })
-      document.addEventListener('keyup', ()=>{
+      document.addEventListener('keyup', ()=> {
         key.classList.remove('active');
       })
 
@@ -155,36 +170,43 @@ window.addEventListener('keydown', (event)=>{
 
       key.addEventListener('mousedown', () =>{
         key.classList.toggle('active');
-        if(buttons[i] === 'Backspace'){
+        if(condition[i] === 'Backspace'){
           textarea.textContent = textarea.textContent.substring(0, textarea.textContent.length-1);
         }
-        else if(buttons[i] === 'Enter'){
+        else if(condition[i] === 'Enter'){
           textarea.textContent += "\n";
         }
-        else if(buttons[i]=== 'Space'){
+        else if(condition[i]=== 'Space'){
           textarea.textContent += ' ';
         }
-        else if(buttons[i] === 'Delete'){
+        else if(condition[i] === 'Delete'){
           textarea.textContent = '';
         }
-        else if(buttons[i]=== 'Tab'){
+        else if(condition[i]=== 'Tab'){
           textarea.textContent += '   ';
         }
-        else if(buttons[i] === 'CapsLock' || buttons[i] === 'ControlRight' || buttons[i] === 'ControlLeft' || buttons[i] === 'ShiftRight' || buttons[i] === 'ShiftLeft' || buttons[i] === 'AltRight' || buttons[i] === 'AltLeft' || buttons[i] === 'Meta' || buttons[i] === 'ArrowDown' || buttons[i] === 'ArrowUp' || buttons[i] === 'ArrowLeft' || buttons[i] === 'ArrowRight'){
-          textarea.textContent = textarea.textContent;
+        else if(condition[i] === 'CapsLock' || condition[i] === 'ControlRight' || condition[i] === 'ControlLeft' || condition[i] === 'ShiftRight' || condition[i] === 'ShiftLeft' || condition[i] === 'AltRight' || condition[i] === 'AltLeft' || condition[i] === 'Meta' || condition[i] === 'ArrowDown' || condition[i] === 'ArrowUp' || condition[i] === 'ArrowLeft' || condition[i] === 'ArrowRight'){
+          textarea.textContent += '';
         }
         else{
-          textarea.textContent += buttons[i];
+          textarea.textContent += condition[i];
         }
       })
       key.addEventListener('mouseup', ()=> {
         key.classList.remove('active');
       })
       
-    };
+    }
       
 
    }
-   addButtons();
+   addButtons(buttons);
+   if(window.addEventListener('keydown', (event) => {event.shiftKey && event.ctrlKey})){
+     
+       addButtons(rusButtons);
+     }
+       
+     
+   
    
 })
